5e4d3634b8d914381007a70c140d1ca43a9a9ace
betterbydesign <3317473+betterbydesign@users.noreply.github.com>
2024-12-26 00:21:40 -0500

    fix: Update AuthForm import in login page to use named import

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,35 +1,9 @@
-import { AuthForm } from '../components/auth/auth-form'
-import Image from 'next/image'
+import { AuthForm } from "../../components/auth/auth-form";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen relative">
-      {/* Background Image */}
-      <div className="fixed inset-0 pointer-events-none">
-        <Image
-          src="/images/signup-bg.svg"
-          alt="Background"
-          fill
-          className="object-cover"
-          priority
-        />
-      </div>
-
-      {/* Logo */}
-      <div className="absolute top-8 left-8">
-        <Image
-          src="/images/veer-logo.svg"
-          alt="Veer"
-          width={120}
-          height={40}
-          priority
-        />
-      </div>
-
-      {/* Auth Form */}
-      <div className="relative z-10 min-h-screen flex items-center justify-center p-4">
-        <AuthForm />
-      </div>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <AuthForm />
     </div>
-  )
+  );
 }
